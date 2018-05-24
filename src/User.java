082b8f6bcc2432a43e13abe0f8650dee094d0c93
seungmun <tjdans174@gmail.com>
2018-05-24 23:31:34 +0900

    fead : User Class

---
+++ src/User.java
@@ -0,0 +1,60 @@
+import java.util.Date;
+
+public class User {
+	private String id;
+	private String password;
+	private String firstname;
+	private String secondname;
+	private String phone;
+	private String email;
+	private String image;
+	private Date joindate;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getSecondname() {
+		return secondname;
+	}
+	public void setSecondname(String secondname) {
+		this.secondname = secondname;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public Date getJoindate() {
+		return joindate;
+	}
+	public void setJoindate(Date joindate) {
+		this.joindate = joindate;
+	}	
+}
