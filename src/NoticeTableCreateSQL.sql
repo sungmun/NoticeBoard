1fcb303ca855b06721abadeaf4b7157629a1d738
seungmun <tjdans174@gmail.com>
2018-05-24 23:30:28 +0900

    fead : Notice Table Create

---
+++ src/NoticeTableCreateSQL.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Notice(
+    notice_num INT(12) unsigned NOT NULL AUTO_INCREMENT,
+    notice_title VARCHAR(50) NOT NULL,
+    member_id  VARCHAR(20) NOT NULL,
+    notice_date datetime,
+    notice_contents VARCHAR(255) NOT NULL,
+    notice_count INT(16) NOT NULL default '0',
+    PRIMARY KEY (notice_num),
+    FOREIGN KEY (member_id) REFERENCES User (user_id) ON DELETE RESTRICT
+);
