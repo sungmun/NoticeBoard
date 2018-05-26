0d365943396409469927905ed47aa51c284f0d82
sungmun <tjdans174@naver.com>
2018-05-26 20:51:35 +0900

    update : Class Name change MySQLDataBaseController => DataAcessObject

---
+++ src/database/DataAcessObject.java
@@ -0,0 +1,25 @@
+package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+
+public class DataAcessObject {
+	
+	protected Connection con;
+	protected PreparedStatement stmt;
+	
+	protected DataAcessObject() throws ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+		try {
+			con = DriverManager.getConnection(PrivateData.dataBaseUrl, PrivateData.dataBaseId, PrivateData.dataBasePassword);
+		} catch(SQLException e) {
+			System.err.println("=============================");
+			System.err.println("SQL Error");
+			System.err.println("=============================");
+		}
+	}
+
+}
