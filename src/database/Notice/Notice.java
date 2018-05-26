1abfd314d8bfbfe529e073ba74cda93039eb70b8
sungmun <tjdans174@naver.com>
2018-05-26 20:52:37 +0900

    update : Notic Class package change

---
+++ src/database/Notice/Notice.java
@@ -0,0 +1,49 @@
+package database.Notice;
+import java.util.Date;
+
+public class Notice {
+	private int notice_num;
+	private String notice_title;
+	private String member_id;
+	private Date notice_date;
+	private String notice_contents;
+	private int notice_count;
+	public int getNotice_num() {
+		return notice_num;
+	}
+	public void setNotice_num(int notice_num) {
+		this.notice_num = notice_num;
+	}
+	public String getNotice_title() {
+		return notice_title;
+	}
+	public void setNotice_title(String notice_title) {
+		this.notice_title = notice_title;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public Date getNotice_date() {
+		return notice_date;
+	}
+	public void setNotice_date(Date notice_date) {
+		this.notice_date = notice_date;
+	}
+	public String getNotice_contents() {
+		return notice_contents;
+	}
+	public void setNotice_contents(String notice_contents) {
+		this.notice_contents = notice_contents;
+	}
+	public int getNotice_count() {
+		return notice_count;
+	}
+	public void setNotice_count(int notice_count) {
+		this.notice_count = notice_count;
+	}
+	
+	
+}
