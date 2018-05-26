8de6d7de6b138afaba9d1ec4b8eb6d6f379299c3
sungmun <tjdans174@naver.com>
2018-05-26 20:49:46 +0900

    update : SQL File Route Change

---
+++ SQLFile/UserInsertSQL.sql
@@ -0,0 +1,2 @@
+INSERT INTO User (user_id,user_password,user_firstname,user_secondname,user_phone,user_email) VALUES ('tjdans174','dkrak174','Kang','Seungmun','010-4160-6443','tjdans174@naver.com');
+
