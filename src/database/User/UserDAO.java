af08d3efcef9abf5061e9d070e8c79ede98d00d7
sungmun <tjdans174@naver.com>
2018-06-06 20:03:56 +0900

    update : add UserDAO Class method errMessageprint

--- src/database/User/UserDAO.java
+++ src/database/User/UserDAO.java
@@ -17,32 +17,31 @@ public class UserDAO extends DataAcessObject {
 		InstanseUserDAO = (InstanseUserDAO == null) ? new UserDAO() : InstanseUserDAO;
 		return InstanseUserDAO;
 	}
-	public User selectUser(String id,String password) {
-		final String SQL="Select * from User where user_id = ? AND user_password = ?";
+
+	public User selectUser(String id, String password) {
+		final String SQL = "Select * from User where user_id = ? AND user_password = ?";
 		try {
 			stmt = con.prepareStatement(SQL);
 			stmt.setString(1, id);
 			stmt.setString(2, password);
-			ResultSet rs=stmt.executeQuery();
+			ResultSet rs = stmt.executeQuery();
 			rs.next();
-			String userId=rs.getString("user_id");
-			String userPassword=rs.getString("user_password");
-			String userFirstname=rs.getString("user_firstname");
-			String userSecondname=rs.getString("user_secondname");
-			String userPhone=rs.getString("user_phone");
-			String userEmail=rs.getString("user_email");
-			String userImage=rs.getString("user_image");
-			Date userJoindate=rs.getDate("joindate");
-			return new User(userId, userPassword, userFirstname, userSecondname, userPhone, userEmail,userImage,userJoindate);
-		}catch (SQLException e) {
-			System.err.println("=======================================");
-			System.err.println("UserDAO.selectUser("+id+","+password+")");
-			System.err.println(e.getMessage());
-			System.err.println(e.getSQLState());
-			System.err.println("=======================================");
+			String userId = rs.getString("user_id");
+			String userPassword = rs.getString("user_password");
+			String userFirstname = rs.getString("user_firstname");
+			String userSecondname = rs.getString("user_secondname");
+			String userPhone = rs.getString("user_phone");
+			String userEmail = rs.getString("user_email");
+			String userImage = rs.getString("user_image");
+			Date userJoindate = rs.getDate("joindate");
+			return new User(userId, userPassword, userFirstname, userSecondname, userPhone, userEmail, userImage,
+					userJoindate);
+		} catch (SQLException e) {
+			errMessageprint(e, "UserDAO.selectUser(" + id + "," + password + ")");
 		}
 		return null;
 	}
+
 	public boolean insertUser(User user) {
 		final String SQL = "INSERT INTO User (user_id,user_password,user_firstname,user_secondname,user_phone,user_email) VALUES (?,?,?,?,?,?)";
 
@@ -57,11 +56,30 @@ public class UserDAO extends DataAcessObject {
 			stmt.execute();
 			return true;
 		} catch (SQLException e) {
-			System.err.println("=======================================");
-			System.err.println("UserDAO.insertUser()");
-			System.err.println(e.getMessage());
-			System.err.println(e.getSQLState());
-			System.err.println("=======================================");
+			errMessageprint(e, "UserDAO.insertUser("+user+")");
+		}
+		return false;
+	}
+
+	public void errMessageprint(Exception e, String callMethod) {
+		System.err.println("=======================================");
+		System.err.println(callMethod);
+		System.err.println(e.getMessage());
+		if (e instanceof SQLException) {
+			System.err.println(((SQLException) e).getSQLState());
+		}
+		System.err.println("=======================================");
+	}
+
+	public boolean isIdDuplicateCheck(String id) {
+		final String SQL = "Select * from User where user_id = ?";
+		try {
+			stmt = con.prepareStatement(SQL);
+			stmt.setString(1, id);
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next())	return true;
+		}catch (SQLException e) {
+			errMessageprint(e, "UserDAO.isIdDuplicateCheck("+id+")");
 		}
 		return false;
 	}
