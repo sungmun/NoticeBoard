e24b8b14b57b6198e344baba354772d8b1c3ec87
sungmun <tjdans174@naver.com>
2018-06-14 20:03:22 +0900

    fead : add WriteServlet class

---
+++ src/servlet/WriteServlet.java
@@ -0,0 +1,53 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import database.Notice.Notice;
+import database.Notice.NoticeDAO;
+import database.User.User;
+
+/**
+ * Servlet implementation class WriteServlet
+ */
+@WebServlet("/Write")
+public class WriteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WriteServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		try {
+			NoticeDAO dao=NoticeDAO.createNoticeDAO();
+			Notice notice=new Notice();
+			notice.setNotice_title(request.getParameter("title"));
+			notice.setNotice_contents(request.getParameter("content"));
+			notice.setMember_id(((User)request.getAttribute("login")).getId());
+			
+			dao.insertNotice(notice);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
