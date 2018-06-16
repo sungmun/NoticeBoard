7c0f2167334b2aca875c158ef634a23ca7a8bc22
sungmun <tjdans174@naver.com>
2018-06-17 01:06:48 +0900

    update : add toString method from to Notice

--- src/database/Notice/Notice.java
+++ src/database/Notice/Notice.java
@@ -1,4 +1,5 @@
 package database.Notice;
+
 import java.util.Date;
 
 public class Notice {
@@ -8,42 +9,59 @@ public class Notice {
 	private Date notice_date;
 	private String notice_contents;
 	private int notice_count;
+
 	public int getNotice_num() {
 		return notice_num;
 	}
+
 	public void setNotice_num(int notice_num) {
 		this.notice_num = notice_num;
 	}
+
 	public String getNotice_title() {
 		return notice_title;
 	}
+
 	public void setNotice_title(String notice_title) {
 		this.notice_title = notice_title;
 	}
+
 	public String getMember_id() {
 		return member_id;
 	}
+
 	public void setMember_id(String member_id) {
 		this.member_id = member_id;
 	}
+
 	public Date getNotice_date() {
 		return notice_date;
 	}
+
 	public void setNotice_date(Date notice_date) {
 		this.notice_date = notice_date;
 	}
+
 	public String getNotice_contents() {
 		return notice_contents;
 	}
+
 	public void setNotice_contents(String notice_contents) {
 		this.notice_contents = notice_contents;
 	}
+
 	public int getNotice_count() {
 		return notice_count;
 	}
+
 	public void setNotice_count(int notice_count) {
 		this.notice_count = notice_count;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "notice_title: " + notice_title + "\n" + "notice_contents: " + notice_contents + "\n"
+		+ "member_id: " + member_id + "\n";
+	}
+
 }
