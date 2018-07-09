6caf988515d41ea9a7cf91a45885ebe6aad1cf2e
sungmun <tjdans174@naver.com>
2018-07-09 20:34:26 +0900

    Fead : add Comment Class (DTO Class)

--- SQLFile/CommentTableCreateSQL.sql
+++ SQLFile/CommentTableCreateSQL.sql
@@ -1,5 +1,4 @@
 CREATE TABLE Comment(
-    comment_num INT(12) unsigned NOT NULL AUTO_INCREMENT,
     notice_num INT(12) unsigned NOT NULL,
     member_id  VARCHAR(20) NOT NULL,
     comment_contents VARCHAR(255) NOT NULL,
