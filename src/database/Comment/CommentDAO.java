3c87c868a8750f9dcba140a3260427509449b71d
sungmun <tjdans174@naver.com>
2018-07-11 20:47:59 +0900

    Fead : Add CommentDAO Class

---
+++ src/database/Comment/CommentDAO.java
@@ -0,0 +1,11 @@
+package database.Comment;
+
+import database.DataAcessObject;
+
+public class CommentDAO extends DataAcessObject{
+
+	protected CommentDAO() throws ClassNotFoundException {
+		super();
+	}
+
+}
