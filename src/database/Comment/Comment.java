951b7de33b21cd54eb17ff5934fb4d33c8abc3fa
sungmun <tjdans174@naver.com>
2018-07-13 21:24:38 +0900

    Update : add toString Method to Comment Class

--- src/database/Comment/Comment.java
+++ src/database/Comment/Comment.java
@@ -1,36 +1,54 @@
 package database.Comment;
 
+import java.lang.reflect.Field;
+
 public class Comment {
-/*
-CREATE TABLE Comment(
-    notice_num INT(12) unsigned NOT NULL,
-    member_id  VARCHAR(20) NOT NULL,
-    comment_contents VARCHAR(255) NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES User (user_id) ON DELETE RESTRICT,
-    FOREIGN KEY (notice_num) REFERENCES Notice (notice_num) ON DELETE CASCADE ON UPDATE CASCADE
-);
- */
+	/*
+	 * CREATE TABLE Comment( notice_num INT(12) unsigned NOT NULL, member_id
+	 * VARCHAR(20) NOT NULL, comment_contents VARCHAR(255) NOT NULL, FOREIGN KEY
+	 * (member_id) REFERENCES User (user_id) ON DELETE RESTRICT, FOREIGN KEY
+	 * (notice_num) REFERENCES Notice (notice_num) ON DELETE CASCADE ON UPDATE
+	 * CASCADE );
+	 */
 	private int noticeNum;
 	private String memberId;
 	private String commentContents;
+
 	public int getNoticeNum() {
 		return noticeNum;
 	}
+
 	public void setNoticeNum(int noticeNum) {
 		this.noticeNum = noticeNum;
 	}
+
 	public String getMemberId() {
 		return memberId;
 	}
+
 	public void setMemberId(String memberId) {
 		this.memberId = memberId;
 	}
+
 	public String getCommentContents() {
 		return commentContents;
 	}
+
 	public void setCommentContents(String commentContents) {
 		this.commentContents = commentContents;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		String json=null;
+		json+="{";
+		
+		json+="\"noticeNum\" : "+noticeNum;
+		
+		json+=", \"memberId\" : "+memberId;
+		json+=", \"commentContents\" : "+commentContents;
+		
+		json+="}";
+		return json;
+	}
 }
