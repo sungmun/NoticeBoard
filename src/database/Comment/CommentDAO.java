0feb62b5be6acfeb5fa176ccba1911ce3cac8c05
sungmun <tjdans174@naver.com>
2018-07-15 21:32:37 +0900

    Update : add Single Pattern to CommentDAO Class

--- src/database/Comment/CommentDAO.java
+++ src/database/Comment/CommentDAO.java
@@ -2,10 +2,15 @@ package database.Comment;
 
 import database.DataAcessObject;
 
-public class CommentDAO extends DataAcessObject{
+public class CommentDAO extends DataAcessObject {
+	private static CommentDAO InstanseCommentDAO;
 
 	protected CommentDAO() throws ClassNotFoundException {
 		super();
 	}
 
+	public static CommentDAO createCommentDAO() throws ClassNotFoundException {
+		InstanseCommentDAO = (InstanseCommentDAO == null) ? new CommentDAO() : InstanseCommentDAO;
+		return InstanseCommentDAO;
+	}
 }
