56e29431817d88daeebec368663fb94b761214ac
sungmun <tjdans174@naver.com>
2018-07-16 17:20:31 +0900

    Update : add getCommentList Method to CommentDAO Class

--- src/database/Comment/CommentDAO.java
+++ src/database/Comment/CommentDAO.java
@@ -1,5 +1,9 @@
 package database.Comment;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import database.DataAcessObject;
 
 public class CommentDAO extends DataAcessObject {
@@ -12,5 +16,25 @@ public class CommentDAO extends DataAcessObject {
 	public static CommentDAO createCommentDAO() throws ClassNotFoundException {
 		InstanseCommentDAO = (InstanseCommentDAO == null) ? new CommentDAO() : InstanseCommentDAO;
 		return InstanseCommentDAO;
+	}
+
+	public ArrayList<Comment> getCommentList(final int post) {
+		String SQL = "SELECT * FROM Comment WHERE notice_num=?";
+		
+		ArrayList<Comment> array = new ArrayList<>();
+		try {
+			stmt = con.prepareStatement(SQL);
+			stmt.setInt(1, post);
+
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Comment comment=new Comment();
+				comment.setCommentContents(rs.getString("comment_contents"));
+				comment.setMemberId(rs.getString("member_id"));
+				array.add(comment);
+			}
+		} catch (SQLException e) {
+		}
+		return array;
 	}
 }
