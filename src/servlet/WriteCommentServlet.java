11a9ed14fd694b2e04f23a21f90d4bf451cb4484
sungmun <tjdans174@naver.com>
2018-07-16 21:40:54 +0900

    Update : add readComment Method to WriteCommentServlet Class

--- src/servlet/WriteCommentServlet.java
+++ src/servlet/WriteCommentServlet.java
@@ -19,43 +19,49 @@ import database.Comment.CommentDAO;
 @WebServlet("/WriteComment")
 public class WriteCommentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public WriteCommentServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		int post=Integer.parseInt(request.getParameter("post"));
-		ArrayList<Comment> list=null;
+	public WriteCommentServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		readComment(request, response);
+	}
+
+
+	public void readComment(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		int post = Integer.parseInt(request.getParameter("post"));
+		ArrayList<Comment> list = null;
 		try {
-			CommentDAO dao=CommentDAO.createCommentDAO();
-			list= dao.getCommentList(post);
+			CommentDAO dao = CommentDAO.createCommentDAO();
+			list = dao.getCommentList(post);
 		} catch (ClassNotFoundException e) {
 		}
-		
-		if(list==null) {
+
+		if (list == null) {
 			return;
 		}
 		response.setCharacterEncoding("UTF-8");
-		PrintWriter write=response.getWriter();
+		PrintWriter write = response.getWriter();
 		for (Comment comment : list) {
 			write.append("<div class=\"post-meta\">");
-				write.append("<div class=\"pull-left\">");
-				write.append("<a href=\"#\">");
-					write.append(comment.getMemberId());
-					write.append("</a>");
-				write.append("</div>");
-				write.append("<div class=\"pull-right\">");
-				write.append("2017-01-01");
-				write.append("</div>");
+			write.append("<div class=\"pull-left\">");
+			write.append("<a href=\"#\">");
+			write.append(comment.getMemberId());
+			write.append("</a>");
+			write.append("</div>");
+			write.append("<div class=\"pull-right\">");
+			write.append("2017-01-01");
+			write.append("</div>");
 			write.append("</div>");
 			write.append("<br>");
 			write.append("<section class=\"panel-body panel-default\">");
@@ -63,5 +69,4 @@ public class WriteCommentServlet extends HttpServlet {
 			write.append("</section>");
 		}
 	}
-
 }
