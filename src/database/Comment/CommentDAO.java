59e54636fac6984e54b64c2d0b42bfddd72833d0
sungmun <tjdans174@naver.com>
2018-07-22 16:51:35 +0900

    Update : add insertComment Method to CommentDAO Class

--- src/database/Comment/CommentDAO.java
+++ src/database/Comment/CommentDAO.java
@@ -1,5 +1,6 @@
 package database.Comment;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -19,7 +20,7 @@ public class CommentDAO extends DataAcessObject {
 	}
 
 	public ArrayList<Comment> getCommentList(final int post) {
-		String SQL = "SELECT * FROM Comment WHERE notice_num=?";
+		String SQL = "SELECT notice_num,member_id,comment_day FROM Comment WHERE notice_num=? order by comment_day";
 		
 		ArrayList<Comment> array = new ArrayList<>();
 		try {
@@ -31,10 +32,29 @@ public class CommentDAO extends DataAcessObject {
 				Comment comment=new Comment();
 				comment.setCommentContents(rs.getString("comment_contents"));
 				comment.setMemberId(rs.getString("member_id"));
+				comment.setCommentDay(rs.getDate("comment_day").toString());
 				array.add(comment);
 			}
 		} catch (SQLException e) {
+			System.err.println(e.getMessage());
 		}
 		return array;
+	}
+	public boolean insertComment(final Comment comment) {
+		String SQL = "INSERT INTO Comment(notice_num,member_id,comment_day,comment_contents) VALUES(?,?,?,?)";
+		
+		try {
+			stmt = con.prepareStatement(SQL);
+			stmt.setInt(1, comment.getNoticeNum());
+			stmt.setString(2, comment.getMemberId());
+			stmt.setDate(3, Date.valueOf(comment.getCommentDay()));
+			stmt.setString(4, comment.getCommentContents());
+
+			stmt.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+			return false;
+		}
 	}
 }
