5d7ad0b263c8911f1f20d3285768019d86fc1993
sungmun <tjdans174@naver.com>
2018-08-07 19:20:16 +0900

    Update : add commentNum, reCommentGroup field to Comment

--- src/database/Comment/Comment.java
+++ src/database/Comment/Comment.java
@@ -2,7 +2,7 @@ package database.Comment;
 
 import java.io.Serializable;
 
-public class Comment implements Serializable{
+public class Comment implements Serializable {
 	/**
 	 * 
 	 */
@@ -12,22 +12,14 @@ public class Comment implements Serializable{
 	 * VARCHAR(20) NOT NULL, comment_contents VARCHAR(255) NOT NULL, FOREIGN KEY
 	 * (member_id) REFERENCES User (user_id) ON DELETE RESTRICT, FOREIGN KEY
 	 * (notice_num) REFERENCES Notice (notice_num) ON DELETE CASCADE ON UPDATE
-	 * CASCADE );
-	 * ALTER TABLE Comment ADD comment_day DATETIME;
+	 * CASCADE ); ALTER TABLE Comment ADD comment_day DATETIME;
 	 */
 	private int noticeNum;
+	private int commentNum;
+	private int reCommentGroup;
 	private String memberId;
 	private String commentContents;
 	private String commentDay;
-	
-	
-	public String getCommentDay() {
-		return commentDay;
-	}
-
-	public void setCommentDay(String commentDay) {
-		this.commentDay = commentDay;
-	}
 
 	public int getNoticeNum() {
 		return noticeNum;
@@ -35,6 +27,22 @@ public class Comment implements Serializable{
 
 	public void setNoticeNum(int noticeNum) {
 		this.noticeNum = noticeNum;
+	}
+
+	public int getCommentNum() {
+		return commentNum;
+	}
+
+	public void setCommentNum(int commentNum) {
+		this.commentNum = commentNum;
+	}
+
+	public int getReCommentGroup() {
+		return reCommentGroup;
+	}
+
+	public void setReCommentGroup(int reCommentGroup) {
+		this.reCommentGroup = reCommentGroup;
 	}
 
 	public String getMemberId() {
@@ -52,4 +60,17 @@ public class Comment implements Serializable{
 	public void setCommentContents(String commentContents) {
 		this.commentContents = commentContents;
 	}
+
+	public String getCommentDay() {
+		return commentDay;
+	}
+
+	public void setCommentDay(String commentDay) {
+		this.commentDay = commentDay;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 }
