43f1ae4a10121ac0826e93de73661d11d66114c8
sungmun <tjdans174@naver.com>
2018-08-12 19:12:47 +0900

    Update : renewal  JsProcessException Class

--- src/Exception/JsProcessException.java
+++ src/Exception/JsProcessException.java
@@ -1,14 +1,24 @@
 package Exception;
+
 import java.io.PrintWriter;
 
-public class JsProcessException extends Exception {
+import com.google.gson.JsonObject;
+
+public class JsProcessException extends RuntimeException {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 4516850744701207841L;
 	PrintWriter write;
-	public JsProcessException(String message) {
-		super(message);
+	String message;
+	public JsProcessException(PrintWriter write,String message) {
+		this.write=write;
+		this.message=message;
 	}
 
+	public void printStackTrace() {
+		JsonObject json = new JsonObject();
+		json.addProperty("err", message);
+		write.println(json);
+	}
 }
