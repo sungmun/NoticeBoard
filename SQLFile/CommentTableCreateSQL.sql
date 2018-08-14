c86d5d5dcbc9124081e03aa8dd18993625d11e3b
sungmun <tjdans174@naver.com>
2018-08-14 18:35:52 +0900

    Update : add Trigger and Alter re_comment_group

--- SQLFile/CommentTableCreateSQL.sql
+++ SQLFile/CommentTableCreateSQL.sql
@@ -12,3 +12,19 @@ CREATE TRIGGER TodayInsertComment BEFORE INSERT ON Comment FOR EACH ROW SET NEW.
 
 ALTER TABLE Comment ADD comment_num INT(12) unsigned NOT NULL auto_increment PRIMARY KEY;
 ALTER TABLE Comment ADD re_comment_group INT(11) unsigned NOT NULL;
+ALTER TABLE Comment MODIFY re_comment_group INT(11) unsigned;
+
+DELIMITER $$
+CREATE TRIGGER ReCommentInsert AFTER
+INSERT ON Comment 
+FOR EACH ROW 
+BEGIN
+IF NEW.re_comment_group IS NULL THEN
+UPDATE Comment
+SET 
+re_comment_group = NEW.comment_num
+WHERE
+comment_num=NEW.comment_num;
+END IF;
+END $$
+DELIMITER ;
