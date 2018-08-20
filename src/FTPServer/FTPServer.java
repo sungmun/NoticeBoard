99c92cf5646e3f01549106f0267a6438791bdd05
sungmun <tjdans174@naver.com>
2018-08-20 20:27:54 +0900

    Update : change constructor method to FTPServer Class

--- src/FTPServer/FTPServer.java
+++ src/FTPServer/FTPServer.java
@@ -12,36 +12,36 @@ import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 
 public class FTPServer {
+
 	public static FTPServer instance;
-	
+
 	FTPClient client;
-	
 	private FTPServer() {
 		client = new FTPClient();
 		client.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
 		int reply;
 		try {
 			client.connect(PrivateData.URL);
-			reply = client.getReplyCode();
-			if (!FTPReply.isPositiveCompletion(reply)) {
+			if (!FTPReply.isPositiveCompletion(client.getReplyCode())) {
 				client.disconnect();
 				throw new IOException("IOException in connecting to FTP Server");
 			}
 			client.login(PrivateData.ID, PrivateData.PASSWORD);
 			client.setFileType(FTP.BINARY_FILE_TYPE);
 			client.enterLocalPassiveMode();
+			client.changeWorkingDirectory("/HDD1/web/NoticeBoard");
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 		}
 	}
 
 	public static FTPServer newInstance() {
-		return (instance==null)?new FTPServer():instance;
+		return (instance == null) ? new FTPServer() : instance;
 	}
 	
-	public void upload(String fileAllPath, String fileName, String hostPath) throws IOException {
-		try (InputStream input = new FileInputStream(new File(fileAllPath))) {
-			this.client.storeFile(hostPath + fileName, input);
+	public void upload(File file, String uploadPath) throws IOException {
+		try (InputStream input = new FileInputStream(file)) {
+			this.client.storeFile(uploadPath, input);
 		}
 	}
 }
