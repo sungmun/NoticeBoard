fb980dd0f92c0553f9d957edd3dd74879e647d5a
sungmun <tjdans174@naver.com>
2018-09-04 22:16:59 +0900

    Update : add FileUpload Part

--- src/servlet/WriteServlet.java
+++ src/servlet/WriteServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import FTPServer.FTPServer;
 import database.Notice.Notice;
 import database.Notice.NoticeDAO;
 import database.User.User;
@@ -41,15 +48,22 @@ public class WriteServlet extends HttpServlet {
 		try {
 			HttpSession session=request.getSession();
 			
+			MultipartRequest multi=new MultipartRequest(request, session.getServletContext().getRealPath("/"),1024*1024*10,"utf-8",new DefaultFileRenamePolicy());
+			
+			
 			NoticeDAO dao=NoticeDAO.createNoticeDAO();
 			Notice notice=new Notice();
 			
-			notice.setNotice_title(request.getParameter("title"));
-			notice.setNotice_contents(request.getParameter("content"));
+			notice.setNotice_title(multi.getParameter("title"));
+			notice.setNotice_contents(multi.getParameter("content"));
 			notice.setMember_id(((User)session.getAttribute("login")).getId());
+			notice.setFile_name(multi.getFilesystemName("upload"));
 			
-			dao.insertNotice(notice);
+			int noticeNum=dao.insertNotice(notice);
 			
+			FTPServer ftp=new FTPServer();
+			ftp.makeDir(Integer.toString(noticeNum));
+			ftp.upload(multi.getFile("upload"), multi.getFilesystemName("upload"));
 			response.sendRedirect("index.jsp");
 			
 		} catch (ClassNotFoundException e) {
