b2b317ecce178c9ed9ae4b90cebf57f9f8b46ff6
sungmun <tjdans174@naver.com>
2018-09-06 21:07:57 +0900

    Update : code Refactoring

--- WebContent/js/indexJs.js
+++ WebContent/js/indexJs.js
@@ -12,7 +12,6 @@
 		var page=$(this).text();
 		maxPageLoad(page);
 		listLoad(page);
-		
 	});
 	
 	$(document).on('click','tbody>tr',function(item){
@@ -39,22 +38,25 @@
 	}
 	
 	function maxPageLoad(page) {
-		var minNum=(page-4<=0)?1:page-4;
+		var minNum=(page-4<=0)?1:page-4;// 최소값 지정(1이 최소이며, 현재 페이지에서 최대 4 작을수 있다)
 		
 		var maxNum=(minNum+9>=maxPage)?maxPage:minNum+9;
+		// 최대값지정(최소값에서 9를 더한 값이며, 그 숫자는 maxPage값을 넘을수 없다)
 		
-		$('li.page-item').remove();
+		$('li.page-item').remove();// 버튼 초기화
 		
 		for(var num=minNum;num<=maxNum;num++){
 			
-			var li=$('<li/>');
-			li.addClass('page-item');
-			if(num == page)	li.addClass('active');
-			li.append($('<a/>',{
+			var li=$('<li/>',{class : 'page-item'});
+			
+			if(num == page)	li.addClass('active');// 현재 페이지에 클래스 추가
+			
+			var a=$('<a/>',{
 				class:'page-link',
 				role:'button',
 				text:num
-				}));
+				});
+			li.append(a);
 			$('.pagination').append(li);
 		}
 		
@@ -62,12 +64,9 @@
 	
 	function listLoad(number) {
 		ajax("/NoticeBoard/ListLoad",{"page":number.trim(), "search":search},function(data){
-			
-			if(data.maxPage!=null)
-				maxPage=data.maxPage;
+			if(data.maxPage!=null)	maxPage=data.maxPage;
 			
 			listChange(JSON.parse(data.list));
-
 		});
 		
 	}
