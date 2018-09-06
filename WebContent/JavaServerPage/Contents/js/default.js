41e1b8aa47121809825c25450357c53cbd324cee
sungmun <tjdans174@naver.com>
2018-09-06 21:08:28 +0900

    Update : code Refactoring

--- WebContent/JavaServerPage/Contents/js/default.js
+++ WebContent/JavaServerPage/Contents/js/default.js
@@ -1,110 +1,87 @@
 	var postNum=$('#post').data('noticenum');
-	commentLoad();
+	
+	//--------------------Comment Part--------------------
+	
+	commentReadAjax();
 
-	function commentWrite(list){
-		var pre_comment=null;
-		var comment_deep_count=0;
-		var comment_deep=40;
-		// margin-left: 40px;
+	function commentWrite(list,callback){
 
 		$('#comment>blockquote').remove();
 		$.each(list,function(index,comment){
+			var total=$('<blockquote/>');
 			
-			var memberid=$('<div/>',{class:'pull-left'});
-			memberid.append($('<a/>',{
-				href:'#',
-				text: comment.memberId
-			}));
-			
-			var commentday=$('<div/>',{
-				class:'pull-right',
-				text: comment.commentDay
-			});
-
-			
-			var post_meat=$('<div/>',{class:'post-meta'});
-			post_meat.append(memberid);
-			post_meat.append(commentday);
-			
-			var commentContent=$('<section/>',{
-				class: 'panel-body panel-default',
-				text: comment.commentContents
-			});
-			
-			var addComment=$('<section/>',{
-				class: 'panel-footer',
-				css: {'font-size':'10px',
-					'padding-top': '3px',
-					'padding-bottom':'15px'
-					}
-			});
-			addComment.append($('<a/>',{
-				class:'pull-right',
-				text:'댓글달기'
-			}));
-// addComment.append($('#comment_input>div>.input-group'))
-			var total=$('<blockquote/>',{
-				css: {'font-size':'13px'}
-			});
 			total.data('comment',comment);
-			if(pre_comment != null&&pre_comment.commentNum==comment.reCommentGroup){
-				comment_deep_count++;
-			}else{
-				comment_deep_count=0;
+			
+			if(comment.commentNum!=comment.reCommentGroup){
+				total.css('margin-left','40px');
 			}
 			
-			total.css('margin-left',(comment_deep*comment_deep_count)+'px');
+			var post_meat=$('<div/>',{class:'post-meta'});
+			post_meat.append($('<div/>',{class:'pull-left',text: comment.memberId}));
+			post_meat.append($('<div/>',{class:'pull-right',text: comment.commentDay}));
 			total.append(post_meat);
+			
 			total.append($('<br/>'));
+			
+			var commentContent=$('<section/>',{class: 'panel-body panel-default', text: comment.commentContents});
 			total.append(commentContent);
-			total.append(addComment);
-			pre_comment=comment;
+			
+			if(comment.commentNum==comment.reCommentGroup){
+				var addComment=$('<section/>',{class: 'panel-footer'});
+				addComment.append($('<div/>',{class:'pull-right',text:'댓글달기'}));
+				total.append(addComment);
+			}
+			
 			$('#comment').append(total);
+			
 		});
 		
 	}
 	
-	$(document).on('click','blockquote #send',function(){
-		var send=$(this).parent();
-		var data={
-				commentContents:'',
-				commentDay:'',
-				commentNum:482,
-				memberId:"tmuzzullo7r",
-				noticeNum:0,
-				reCommentGroup:482,
-		}
-	});
-	
 	var footer=null;
-	$(document).on('click','blockquote>.panel-footer>a',function(){
+	$(document).on('click','blockquote>.panel-footer>.pull-right',function(){
 		if(footer==null){
 			footer=$(this).parent();
 		}else{
 			footer.text('');
-			footer.css( {'font-size':'10px',
-				'padding-top': '3px',
-				'padding-bottom':'15px'
-				});
-			footer.append($('<a/>',{
+			footer.css('padding-bottom','15px');
+			footer.append($('<div/>',{
 				class:'pull-right',
 				text:'댓글달기'
 			}));
+			
 			footer=$(this).parent();
 		}
-		footer.css({
-			'padding-top': '5px',
-			'padding-bottom':'5px'
-		})
 		footer.text('');
+		footer.css('padding-bottom','5px');
 		$('#comment_input>div>.input-group').clone().appendTo(footer);
+		footer.find('#comment-area').val('');
 	});
 	
-	$("#send").click(function() {
-		ajax("/NoticeBoard/WriteComment",{post:postNum,contents:$('#comment-area').val()},(json)=>showErrorModal('경고',json.err));
-		commentLoad('${notice.notice_num}');
+	$(document).on('click','blockquote #send',function(){
+		var send=$(this).parent().parent().parent();
+		
+		var data={
+			reComment:send.data('comment').commentNum,
+			post:postNum,
+			contents:send.find('#comment-area').val()
+		}
+		commentWriteAjax(data);
 	});
 	
-	function commentLoad() {
-		ajax("/NoticeBoard/ReadComment",{"post":postNum},(json)=>commentWrite(JSON.parse(json.list)));
+	$(document).on('click','#comment_input #send',function() {
+		var comments=$(this).parent().find('#comment-area');
+		commentWriteAjax({post:postNum,contents:comments.val()});
+	});
+	
+	function commentWriteAjax(data){
+		ajax("/NoticeBoard/WriteComment",data,(json)=>showErrorModal('경고',json.err));
+		commentReadAjax();	
+	}
+	
+	function commentReadAjax() {
+		ajax("/NoticeBoard/ReadComment",
+				{"post":postNum},
+				(json)=>commentWrite(JSON.parse(json.list)));
+		//(DOM)=>$('#comment').append(DOM)
 	}
